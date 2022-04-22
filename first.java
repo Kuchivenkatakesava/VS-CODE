4be679578eca58c7b4c23a9e2d2898119f7a86fa
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-04-22 16:26:57 +0530

    test

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first{
+    //main method
+    public static void main (String[] args){
+        double a = 50.9;
+        int b = -6;
+          
+          system.out.println("sum =" + (a+b));
+          system.out.println("subtraction =" + (a-b));
+          system.out.println("Division =" +(a/b));
+          system.out.println("product =" +(a*b));
+    }
+}          
