e5b7e07e9a04b9199164c8be8539f6d8c1b6492e
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-04-23 14:52:09 +0530

    test

--- task.java
+++ task.java
@@ -1,3 +1,16 @@
 package Basics;
 public class task {
-    public static void main
+    public static void main (String[] args){
+        string s="kesava";
+        system.out.println(x: "Enter the age");
+          scanner sc = new scanner(system.in);
+          int age = sc.nextint();
+          if(age >= 18){
+              system.out.println(s+" "+" is eligible");{
+                  else{
+                      system.out.println(s+ " "+" is not eligible");
+                  }
+
+              }
+
+          }                            
