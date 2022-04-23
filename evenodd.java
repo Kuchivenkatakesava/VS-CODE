a8b60d97a96f2ab8b5a7fccca825a7d9e75e9f85
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-04-23 15:09:30 +0530

    evenodd

---
+++ evenodd.java
@@ -0,0 +1,20 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+
+        // n % 2 == 0 (even)
+        // n % 2 not = 0 (odd)
+
+        if(n % 2 == 0) {
+            System.out.println(n + " is Even");
+        } else {
+            System.out.println(n + " is Odd");
+        }
+    }
+}
