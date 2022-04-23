c7781eb61eec9f5c798f116722c539e39f299a1e
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-04-23 15:03:27 +0530

    datatypes

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class datatypes {
+    public static void main(String[] args) {
+        // numbers -> Integers
+        int a = -11;
+
+        // point, decimal value
+        double d = 47.6;
+
+        // Word, Sentence
+        String s = "kesava is the Trainer";
+
+        // Character
+        char ch = 'a';
+
+        // true, false
+        boolean bool = true;
+    }
+}
