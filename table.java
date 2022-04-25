143532c8a08fed51e253a569cbf8ca8ad81b1d03
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-04-25 14:48:17 +0530

    table

---
+++ table.java
@@ -0,0 +1,15 @@
+import java.until.Scanner;
+public class table {
+    public static void main(String[] args) {
+        scanner sc = new scanner(system.in);
+        system.out.println("enter number:");
+        int num=sc.nextint();
+        for (int i=1;i<=10;i++) {
+            system.out.println(num+"*"+i+"="+num*i);
+        }
+    }
+}            
+
+
+
+    
