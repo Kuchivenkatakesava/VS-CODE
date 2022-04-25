29c4de8b2e984974b8734769ab2bfe782f425af2
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-04-25 15:42:07 +0530

    whileloop

--- table.java
+++ table.java
@@ -1,11 +1,10 @@
-import java.until.Scanner;
+
 public class table {
     public static void main(String[] args) {
-        scanner sc = new scanner(system.in);
-        system.out.println("enter number:");
-        int num=sc.nextint();
-        for (int i=1;i<=10;i++) {
-            system.out.println(num+"*"+i+"="+num*i);
+        int num = 5;
+        for(int i=1; i<=10; ++i) {
+            System.out.printf("%d * %d = % \n", num,i num * i );
+                    
         }
     }
 }            
