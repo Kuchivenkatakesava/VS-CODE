79964c90604e568c5aa5c5cffd8d7c713e223cbf
Venkata Kesava Kuchi <v.kuchi@zensar.com>
2022-05-10 10:52:39 +0530

    odd length

---
+++ Core_Java/0ddlength.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class oddlength 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String arr[] = new String[n];
+
+        for(int i=0;i<n;i++)
+        arr[i]=sc.next();
+
+        int len=0;
+        ArrayList<String> oddlength=new ArrayList<String>();
+        for (int i=0;i<n;i++)
+        {
+            len=arr[i].length();
+            if(len%2==1)
+            oddlength.add(arr[i]);
+
+        }
+        if(oddlength.size()==0)
+           System.out.println("Better Luck Next time");
+           
+        else
+        {
+            iterator itr=oddlength.iterator();
+            int max=-1;
+            String res="";
+            while(itr.hasNext())
+            {
+                String temp=(String)itr.next();
+                if(temp.length()>max) 
+                {
+                    res=temp;
+                    max=temp.length();
+                }
+            }
+            System.out.println(res);
+        }
+    }                  
+}
